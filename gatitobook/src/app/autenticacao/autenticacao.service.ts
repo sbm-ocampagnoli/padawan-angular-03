ba06d7fdae25a107be07422e7711cc042e034fe5
sbm-ocampagnoli <otavio.campagnoli@sbmtechnology.com.br>
2024-06-13 14:32:28 -0300

    Environment.

--- gatitobook/src/app/autenticacao/autenticacao.service.ts
+++ gatitobook/src/app/autenticacao/autenticacao.service.ts
@@ -3,6 +3,9 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UsuarioService } from './usuario/usuario.service';
 import { tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment.prod';
+
+const API = environment.apiURL;
 
 @Injectable({
   providedIn: 'root',
@@ -11,12 +14,12 @@ export class AutenticacaoService {
   constructor(
     private httpClient: HttpClient,
     private usuarioService: UsuarioService
-  ) {}
+  ) { }
 
   autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
     return this.httpClient
       .post(
-        'http://localhost:3000/user/login',
+        `${API}/user/login`,
         {
           userName: usuario,
           password: senha,
