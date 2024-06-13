a62367861ead696909d9b95ccc3cdd3f7eabdd7f
sbm-ocampagnoli <otavio.campagnoli@sbmtechnology.com.br>
2024-06-13 14:54:45 -0300

    Grade de fotos dos animais.

--- gatitobook/src/app/componentes/cartao/cartao.component.ts
+++ gatitobook/src/app/componentes/cartao/cartao.component.ts
@@ -8,7 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class CartaoComponent implements OnInit {
   @Input() titulo = '';
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 }
