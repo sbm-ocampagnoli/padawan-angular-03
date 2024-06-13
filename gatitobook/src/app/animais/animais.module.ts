7fffd00ab2c886af9f34df1f88ee5e7027854b95
sbm-ocampagnoli <otavio.campagnoli@sbmtechnology.com.br>
2024-06-13 15:47:04 -0300

    Projeto da aula anterior.

--- gatitobook/src/app/animais/animais.module.ts
+++ gatitobook/src/app/animais/animais.module.ts
@@ -7,13 +7,8 @@ import { AnimalComponent } from './animal/animal.component';
 import { CartaoModule } from '../componentes/cartao/cartao.module';
 import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
 
-
 @NgModule({
   declarations: [ListaAnimaisComponent, AnimalComponent, GradeFotosAnimaisComponent],
-  imports: [
-    CommonModule,
-    AnimaisRoutingModule,
-    CartaoModule
-  ]
+  imports: [CommonModule, AnimaisRoutingModule, CartaoModule],
 })
-export class AnimaisModule { }
+export class AnimaisModule {}
